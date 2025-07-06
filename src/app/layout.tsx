091e85e1c3a9aa226cb112e3ee7ee67301901a7d
Septem Li <septemhill@gmail.com>
2025-07-06 18:30:41 +0800

    feat: Add sonner for toast notifications
    This commit adds the `sonner` library to provide toast notifications.
    The `Toaster` component is added to the root layout to display notifications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClientLayout } from "./ClientLayout";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClientLayout>{children}</ClientLayout>
+        <Toaster />
       </body>
     </html>
   );
